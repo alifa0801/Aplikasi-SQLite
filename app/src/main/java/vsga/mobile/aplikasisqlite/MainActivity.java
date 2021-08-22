c1eeb76345774d6e0b9037618633704a305bfe7b
Lenovo <ifaalifa24@gmail.com>
2021-08-23 01:06:18 +0700

    Initial commits

---
+++ app/src/main/java/vsga/mobile/aplikasisqlite/MainActivity.java
@@ -0,0 +1,188 @@
+package vsga.mobile.aplikasisqlite;
+
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.view.MenuInflater;
+import android.view.View;
+
+import androidx.appcompat.widget.Toolbar;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import vsga.mobile.aplikasisqlite.adapter.Adapter;
+import vsga.mobile.aplikasisqlite.databinding.ActivityMainBinding;
+import vsga.mobile.aplikasisqlite.helper.DbHelper;
+import vsga.mobile.aplikasisqlite.model.Data;
+
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.AdapterView;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    ListView listView;
+    AlertDialog.Builder dialog;
+    List<Data> itemList = new ArrayList<Data>();
+    Adapter adapter;
+    DbHelper SQLite = new DbHelper(this);
+
+    public static final String TAG_ID = "id";
+    public static final String TAG_NAME = "name";
+    public static final String TAG_ADDRESS = "address";
+
+    private AppBarConfiguration appBarConfiguration;
+    private ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        SQLite = new DbHelper(getApplicationContext());
+        FloatingActionButton fab = findViewById(R.id.fab);
+        listView = (ListView) findViewById(R.id.list_view);
+
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, AddEdit.class);
+                startActivity(intent);
+            }
+        });
+
+        adapter = new Adapter(MainActivity.this, itemList);
+        listView.setAdapter(adapter);
+
+        listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(final AdapterView<?> parent, View view, final int position, long id) {
+                final String idx = itemList.get(position).getId();
+                final String name = itemList.get(position).getName();
+                final String address = itemList.get(position).getAddress();
+
+                final CharSequence[] dialogitem = {"Edit", "Delete"};
+                dialog = new AlertDialog.Builder(MainActivity.this);
+                dialog.setCancelable(true);
+                dialog.setItems(dialogitem, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        switch (which) {
+                            case 0:
+                                Intent intent = new Intent(MainActivity.this, AddEdit.class);
+                                intent.putExtra(TAG_ID, idx);
+                                intent.putExtra(TAG_NAME, name);
+                                intent.putExtra(TAG_ADDRESS, address);
+                                startActivity(intent);
+                                break;
+                            case 1:
+                                SQLite.delete(Integer.parseInt(idx));
+                                itemList.clear();
+                                getAllData();
+                                break;
+                        }
+                    }
+                }).show();
+                return false;
+            }
+        });
+        getAllData();
+
+//        binding = ActivityMainBinding.inflate(getLayoutInflater());
+//        setContentView(binding.getRoot());
+//
+//        setSupportActionBar(binding.toolbar);
+//
+//        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
+//        appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
+//        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+//
+//        binding.fab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+//            }
+//        });
+
+
+    }
+
+    private void getAllData() {
+        ArrayList<HashMap<String, String>> row = SQLite.getAllData();
+
+        for (int i = 0; i < row.size(); i++){
+            String id = row.get(i).get(TAG_ID);
+            String poster = row.get(i).get(TAG_NAME);
+            String title = row.get(i).get(TAG_ADDRESS);
+
+            Data data = new Data();
+
+            data.setId(id);
+            data.setName(poster);
+            data.setAddress(title);
+
+            itemList.add(data);
+        }
+        adapter.notifyDataSetChanged();
+    }
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+        itemList.clear();
+        getAllData();
+    }
+
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//        // Inflate the menu; this adds items to the action bar if it is present.
+//        getMenuInflater().inflate(R.menu.menu_main, menu);
+//        return true;
+//    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+//    @Override
+//    public boolean onSupportNavigateUp() {
+//        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
+//        return NavigationUI.navigateUp(navController, appBarConfiguration)
+//                || super.onSupportNavigateUp();
+//    }
+}
